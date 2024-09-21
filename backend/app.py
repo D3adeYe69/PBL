5a8756a063e74df7dfc0a8a524aa34b396ac846f
D3adeYe69 <killer1x4@gmail.com>
2024-09-21 16:56:07 +0300

    new

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, jsonify
-from project import create_app
+from flask import Flask, jsonify, url_for, render_template,request
+from project import create_app,db
+
 app = create_app()
 
-@app.route('/', methods=['GET'])
-def get_data():
-    
 
-    
-    return jsonify({'message': 'hello website'})
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    data = request.json
+    new_user = User(username=data['username'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created!'}), 201
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
