4f9631202d31113be25f22b5c4da95673c0cd111
Liviu C. <106706572+D3adeYe69@users.noreply.github.com>
2024-11-24 23:49:21 +0200

    Update settings.py

--- habits/habits/settings.py
+++ habits/habits/settings.py
@@ -94,9 +94,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'killer1x4@gmail.com'
-EMAIL_HOST_PASSWORD = 'woew arwu adxq jqxf'
-DEFAULT_FROM_EMAIL = 'killer1x4@gmail.com'
+EMAIL_HOST_USER = 'djangoproject31@gmail.com'
+EMAIL_HOST_PASSWORD = 'azij bdwu dtlg wrww'
+DEFAULT_FROM_EMAIL = 'djangoproject31@gmail.com'
 
 # Add these settings
 SITE_ID = 1
